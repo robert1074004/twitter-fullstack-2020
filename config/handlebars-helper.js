d2a34414180a7b32eb14b1f56b521bbba7993ad7
MerryHao <mq6721@gmail.com>
2023-04-03 21:42:09 +0800

    feat: font color will change due to each page

--- config/handlebars-helper.js
+++ config/handlebars-helper.js
@@ -26,5 +26,8 @@ module.exports = {
   },
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
-  }
+  },
+  ifNotCond: function (a, b, options) {
+    return a === b ? options.inverse(this) : options.fn(this)
+  },
 };
